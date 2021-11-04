e46793ed79724c1964994d5350efa6eef9e6b8e2
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2021-11-04 15:55:44 +0100

    nn prefix for layer ref

--- nn/transformer.py
+++ nn/transformer.py
@@ -5,7 +5,6 @@ Transformer Modules
 from __future__ import annotations
 from typing import Optional, Any, Union, Callable
 from .. import nn
-from . import LayerRef
 
 
 class TransformerEncoderLayer(nn.Module):
@@ -13,7 +12,7 @@ class TransformerEncoderLayer(nn.Module):
   Defines one layer of a standard transformer encoder
   """
   def __init__(self, dim_model: int, num_heads: int, dim_ff: int = 2048, dropout: float = 0.1,
-               activation: Callable[[LayerRef], LayerRef] = nn.relu, layer_norm_eps: float = 1e-5,
+               activation: Callable[[nn.LayerRef], nn.LayerRef] = nn.relu, layer_norm_eps: float = 1e-5,
                norm_first: bool = False) -> None:
     """
     :param dim_model: hidden dim, PyTorch name: d_model
@@ -35,7 +34,7 @@ class TransformerEncoderLayer(nn.Module):
     self.norm_eps = layer_norm_eps
     self.dropout = dropout
 
-  def forward(self, inp: LayerRef) -> LayerRef:
+  def forward(self, inp: nn.LayerRef) -> nn.LayerRef:
     """
     Two possible forward variants of encoder, defined by self.norm_first
     """
@@ -49,11 +48,11 @@ class TransformerEncoderLayer(nn.Module):
 
     return x
 
-  def _self_attention_block(self, x: LayerRef) -> LayerRef:
+  def _self_attention_block(self, x: nn.LayerRef) -> nn.LayerRef:
     x = self.self_attn(x, x, x)
     return nn.dropout(x, self.dropout)
 
-  def _feed_forward_block(self, x: LayerRef) -> LayerRef:
+  def _feed_forward_block(self, x: nn.LayerRef) -> nn.LayerRef:
     x = self.linear_ff(x)
     x = self.activation(x)
     x = nn.dropout(x, dropout=self.dropout)
@@ -67,7 +66,7 @@ class TransformerEncoder(nn.Module):
   Defines the full Encoder of the standard transformer
   """
   def __init__(self, encoder_layer: Union[TransformerEncoderLayer, Any], num_layers: int,
-               norm: Callable[[LayerRef], LayerRef] = None, layer_norm_eps: float = 1e-5):
+               norm: Callable[[nn.LayerRef], nn.LayerRef] = None, layer_norm_eps: float = 1e-5):
     """
     :param encoder_layer: Encoder layer to be stacked num_layers times
     :param num_layers: Number of layers
@@ -82,7 +81,7 @@ class TransformerEncoder(nn.Module):
     self.norm = norm
     self.norm_eps = layer_norm_eps
 
-  def forward(self, inp: LayerRef) -> LayerRef:
+  def forward(self, inp: nn.LayerRef) -> nn.LayerRef:
     """
     Executes the encoder layer as often as in num_layers defined
     """
@@ -101,7 +100,7 @@ class TransformerDecoderLayer(nn.Module):
   Defines one layer of a standard transformer decoder
   """
   def __init__(self, dim_model: int, num_heads: int, dim_ff: int = 2048, dropout: float = 0.1,
-               activation: Callable[[LayerRef], LayerRef] = nn.relu, layer_norm_eps: float = 1e-5,
+               activation: Callable[[nn.LayerRef], nn.LayerRef] = nn.relu, layer_norm_eps: float = 1e-5,
                norm_first: bool = False):
     """
     :param dim_model: hidden dim, PyTorch name: d_model
@@ -124,7 +123,7 @@ class TransformerDecoderLayer(nn.Module):
     self.activation = activation
     self.dropout = dropout
 
-  def forward(self, tgt: LayerRef, memory: LayerRef) -> LayerRef:
+  def forward(self, tgt: nn.LayerRef, memory: nn.LayerRef) -> nn.LayerRef:
     """
     Two possible forward variants of decoder, defined by self.norm_first
     """
@@ -140,15 +139,15 @@ class TransformerDecoderLayer(nn.Module):
 
     return x
 
-  def _self_attention_block(self, x: LayerRef) -> LayerRef:
+  def _self_attention_block(self, x: nn.LayerRef) -> nn.LayerRef:
     x = self.self_attn(x, x, x)  # will change with Attention Modules
     return nn.dropout(x, self.dropout)
 
-  def _multi_head_attention_block(self, x: LayerRef, mem: LayerRef) -> LayerRef:
+  def _multi_head_attention_block(self, x: nn.LayerRef, mem: nn.LayerRef) -> nn.LayerRef:
     x = self.multihead_attn(x, mem, mem)  # will change with Attention Modules
     return nn.dropout(x, self.dropout)
 
-  def _feed_forward_block(self, x: LayerRef) -> LayerRef:
+  def _feed_forward_block(self, x: nn.LayerRef) -> nn.LayerRef:
     x = self.linear_ff(x)
     x = self.activation(x)
     x = nn.dropout(x, dropout=self.dropout)
@@ -162,7 +161,7 @@ class TransformerDecoder(nn.Module):
   Defines the full Decoder of the standard transformer
   """
   def __init__(self, decoder_layer: Union[TransformerDecoderLayer, Any], num_layers: int,
-               norm: Callable[[LayerRef], LayerRef] = None, layer_norm_eps: float = 1e-5):
+               norm: Callable[[nn.LayerRef], nn.LayerRef] = None, layer_norm_eps: float = 1e-5):
       """
       :param decoder_layer: Decoder layer to be stacked num_layers times
       :param num_layers: Number of layers
@@ -176,7 +175,7 @@ class TransformerDecoder(nn.Module):
       self.norm = norm
       self.norm_eps = layer_norm_eps
 
-  def forward(self, tgt: LayerRef, memory: LayerRef) -> LayerRef:
+  def forward(self, tgt: nn.LayerRef, memory: nn.LayerRef) -> nn.LayerRef:
     """
     Executes the decoder layer as often as in num_layers defined
     """
@@ -196,7 +195,7 @@ class Transformer(nn.Module):
   """
   def __init__(self, dim_model: int = 512, num_heads: int = 8, num_encoder_layers: int = 6,
                num_decoder_layers: int = 6, dim_ff: int = 2048, drop: float = 0.1,
-               activation: Callable[[LayerRef], LayerRef] = nn.relu,
+               activation: Callable[[nn.LayerRef], nn.LayerRef] = nn.relu,
                custom_encoder: Optional[Any] = None, custom_decoder: Optional[Any] = None,
                layer_norm_eps: float = 1e-5) -> None:
     """
@@ -229,7 +228,7 @@ class Transformer(nn.Module):
     self.dim_model = dim_model
     self.num_heads = num_heads
 
-  def forward(self, src: LayerRef, tgt: LayerRef) -> LayerRef:
+  def forward(self, src: nn.LayerRef, tgt: nn.LayerRef) -> nn.LayerRef:
     """
     Forward step of Transformer
     """
